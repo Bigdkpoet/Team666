8e7c261390d5aad4786d1d26a7044d8910e73503
JojiChan <ggc1@sfu.ca>
2019-10-17 14:10:57 -0700

    Updating the CSS, Js file.
    Edited old pages , making new page

---
+++ js/addbag.js
@@ -0,0 +1,123 @@
+     $("#small_card1").hover(function(){
+              
+               // $("#product1").attr("src","img/human.jpg");
+                $(".product-card-image").css("opacity","1");
+                $("#small_card1").css("opacity","1");
+                $("#small_card4").css("opacity","0.5");
+                $("#small_card3").css("opacity","0.5");
+                $("#small_card2").css("opacity","0.5");
+              
+                $(".product-card-image2").css("opacity","0");
+                 $(".product-card-image3").css("opacity","0");
+                  $(".product-card-image4").css("opacity","0");
+             },function(){
+               $(".product-card-image").css("opacity","1");
+               $(".product-card-image2").css("opacity","0");
+            $(".product-card-image3").css("opacity","0");
+               $("#small_card1").css("opacity","1");
+                $("#small_card4").css("opacity","0.5");
+                $("#small_card3").css("opacity","0.5");
+                $("#small_card2").css("opacity","0.5");
+                $(".product-card-image4").css("opacity","0");
+
+            });
+          $("#small_card2").hover(function(){
+              
+               // $("#product1").attr("src","img/human.jpg");
+                $(".product-card-image").css("opacity","0");
+                $("#small_card2").css("opacity","1");
+                $("#small_card3").css("opacity","0.5");
+                $("#small_card1").css("opacity","0.5");
+                $("#small_card4").css("opacity","0.5");
+                $(".product-card-image2").css("opacity","1");
+                 $(".product-card-image3").css("opacity","0");
+                  $(".product-card-image4").css("opacity","0");
+               
+                
+               // $("#brd2").html("$605.00 CAD");
+               //  $("#sep2").html(" ");
+             },function(){
+              
+              // $("#product1").attr("src","img/jacket.jpg");
+               $(".product-card-image").css("opacity","0");
+               $("#small_card2").css("opacity","1");
+               $(".product-card-image2").css("opacity","1");
+                 $("#small_card2").css("opacity","1");
+                $("#small_card3").css("opacity","0.5");
+                $("#small_card1").css("opacity","0.5");
+                $("#small_card4").css("opacity","0.5");
+            $(".product-card-image3").css("opacity","0");
+                  $(".product-card-image4").css("opacity","0");
+
+              
+
+            });
+
+
+              $("#small_card3").hover(function(){
+              
+               // $("#product1").attr("src","img/human.jpg");
+                $(".product-card-image").css("opacity","0");
+                $("#small_card3").css("opacity","1");
+                  $("#small_card3").css("opacity","1");
+                $("#small_card2").css("opacity","0.5");
+                $("#small_card1").css("opacity","0.5");
+                $("#small_card4").css("opacity","0.5");
+                $(".product-card-image2").css("opacity","0");
+                 $(".product-card-image3").css("opacity","1");
+                  $(".product-card-image4").css("opacity","0");
+               
+                
+               // $("#brd2").html("$605.00 CAD");
+               //  $("#sep2").html(" ");
+             },function(){
+              
+              // $("#product1").attr("src","img/jacket.jpg");
+               $(".product-card-image").css("opacity","0");
+               $(".product-card-image2").css("opacity","0");
+               $("#small_card3").css("opacity","1");
+                    $("#small_card3").css("opacity","1");
+                $("#small_card2").css("opacity","0.5");
+                $("#small_card1").css("opacity","0.5");
+                $("#small_card4").css("opacity","0.5");
+            $(".product-card-image3").css("opacity","1");
+                  $(".product-card-image4").css("opacity","0");
+
+              
+
+            });
+   $("#small_card4").hover(function(){
+              
+               // $("#product1").attr("src","img/human.jpg");
+                $(".product-card-image").css("opacity","0");
+                $(".product-card-image2").css("opacity","0");
+                $("#small_card4").css("opacity","1");
+                $("#small_card3").css("opacity","0.5");
+                $("#small_card2").css("opacity","0.5");
+                $("#small_card1").css("opacity","0.5");
+       
+                 $(".product-card-image3").css("opacity","0");
+                  $(".product-card-image4").css("opacity","1");
+               
+                
+               // $("#brd2").html("$605.00 CAD");
+               //  $("#sep2").html(" ");
+             },function(){
+              
+              // $("#product1").attr("src","img/jacket.jpg");
+               $(".product-card-image").css("opacity","0");
+               $("#small_card4").css("opacity","1");
+                $("#small_card3").css("opacity","0.5");
+                $("#small_card2").css("opacity","0.5");
+                $("#small_card1").css("opacity","0.5");
+               $(".product-card-image2").css("opacity","0");
+            $(".product-card-image3").css("opacity","0");
+            $("#small_card4").css("opacity","1");
+                  $(".product-card-image4").css("opacity","1");
+
+              
+
+            });
+
+
+
